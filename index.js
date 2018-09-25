eb04c0df0c940e944ee83a6ccb505c2e384f47d2
tburnam <tyler@burnamtech.com>
2018-09-25 10:27:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,28 +2,31 @@ const winston = require('winston');
 const util = require('util');
 
 exports = module.exports = FirebaseLogger = winston.transports.FirebaseLogger = function (options) {
-	this.name = options.name || 'FirebaseLogger';
-	this.level = options.level || 'silly';
-	this.ref = options.ref || 'logs';
-	this.key = options.key;
+  this.name = options.name || 'FirebaseLogger';
+  this.level = options.level || 'silly';
+  this.ref = options.ref || 'logs';
+  this.key = options.key;
 };
 
 util.inherits(FirebaseLogger, winston.Transport);
 
 FirebaseLogger.prototype.log = function (level, msg, meta, callback) {
-	const key = meta[this.key] || Date.now();
+  const key = meta[this.key] || Date.now();
 
-	this.ref.update({
-		[key]: {
-			level: level,
-			msg: msg,
-			meta: meta
-		}
-	}, error => {
-		if (error) {
-			callback(error);
-		} else {
-			callback(null, true);
-		}
-	});
+  this.ref.update({
+    [key]: {
+      level,
+      message: msg,
+      timestamp: Date.now(),
+      file: meta.file || '',
+      action: meta.action || '',
+      id: meta.id || '',
+    },
+  }, (error) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, true);
+    }
+  });
 };
